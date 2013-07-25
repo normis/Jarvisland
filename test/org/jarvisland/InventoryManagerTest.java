446787a58b9c32f3433ad227d462d8397bd6f77b
Nicolas Lupien <niclupien@gmail.com>
2013-07-25 12:10:03 -0400

    adding a test

---
+++ test/org/jarvisland/InventoryManagerTest.java
@@ -0,0 +1,15 @@
+package org.jarvisland;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InventoryManagerTest {
+
+	@Test
+	public void testAddItem() {
+		InventoryManager.getInstance().addItem("item");
+		assertTrue(InventoryManager.getInstance().hasItem("item"));
+	}
+
+}
