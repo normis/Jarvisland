677d4ad1452f0b2f5aa7a266cbda803e6e35d055
doodlegoo <vincent.gagnon5@usherbrooke.ca>
2013-07-31 11:26:17 -0400

    Extend console test

--- test/org/jarvisland/ConsoleTest.java
+++ test/org/jarvisland/ConsoleTest.java
@@ -2,17 +2,29 @@ package org.jarvisland;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class ConsoleTest {
 
 	@Test
 	public void testVoir() {
-		String s = "a";
-		//new JarvislandEngine(System.in,System.out);
+		String in = "voir";
+		String out = "log.txt";
 		
+		//try {
+			new JarvislandEngine(System.in, System.out);
+			//new JarvislandEngine((InputStream) new ByteArrayInputStream(in.getBytes()),new PrintStream(out) );
+		//} catch (FileNotFoundException e) {
+			//e.printStackTrace();
+		//}
 		
-		assertEquals(s, "a");
+		System.out.println(out);
+		//assertEquals(, "a");
 		//fail("Not yet implemented");
 	}
 
